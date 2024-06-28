e56814e6f61f0f6231d9d28c7351e81f6b9b320e
nelson2191 <57192468+nelson2191@users.noreply.github.com>
2024-06-27 20:51:35 -0600

    Add files via upload

---
+++ spotify/Programacion.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spotify;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Programacion {
+    public String idCancion="";
+    public String title="";
+    public String author="";
+    public int lenght=0;
+    public int year=0;
+
+    public String getIdCancion() {
+        return idCancion;
+    }
+
+    public void setIdCancion(String idCancion) {
+        this.idCancion = idCancion;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getLenght() {
+        return lenght;
+    }
+
+    public void setLenght(int lenght) {
+        this.lenght = lenght;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+    
+    
+    
+}
---
+++ spotify/Spotify.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package spotify;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Spotify {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Programacion spotify=new Programacion();
+        spotify.setIdCancion("hds001");
+        spotify.getIdCancion();
+    }
+    
+}
